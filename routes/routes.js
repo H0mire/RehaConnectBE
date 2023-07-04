4c94b6e23c845d4ea551fcd61ade98e55752eff0
H0mire <janikmueller12@gmail.com>
2023-07-04 19:32:43 +0200

    added one route

---
+++ routes/routes.js
@@ -0,0 +1,16 @@
+import express from "express";
+import db from "../db/conn.mjs";
+import { ObjectId } from "mongodb";
+
+const router = express.Router();
+
+router.get("users", async (req, res) => {
+	let collection = await db.collection("users");
+	let results = await collection.find({})
+	  .limit(50)
+	  .toArray();
+  
+	res.send(results).status(200);
+});
+
+export default router;
