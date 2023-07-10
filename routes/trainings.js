31d5f8f51ae6f59c9412b4210e46bbd68e11e70b
H0mire <janikmueller12@gmail.com>
2023-07-10 12:35:44 +0200

    added training functionality

--- routes/trainings.js
+++ routes/trainings.js
@@ -15,4 +15,53 @@ router.get("/:id", async (req, res) => {
   }
 });
 
+router.get("/pulse/:id", async (req, res) => {
+	try {
+		let pulseData = [
+			{ time: 0, pulse: 80 },
+			{ time: 1, pulse: 88 },
+			{ time: 2, pulse: 96 },
+			{ time: 3, pulse: 104 },
+			{ time: 4, pulse: 112 },
+			{ time: 5, pulse: 120 },
+		];
+		
+		let currentPulse = 120; // Ausgangspunkt für Variation
+		for (let time = 6; time <= 60; time++) {
+			// Füge Variation hinzu, indem ein zufälliger Wert zwischen -5 und 5 zum aktuellen Puls hinzugefügt wird
+			let variation = Math.floor(Math.random() * 21) - 10;
+			currentPulse += variation;
+			pulseData.push({ time: time, pulse: currentPulse });
+		}
+	  res.status(200).json(pulseData);
+	} catch (error) {
+	  res.status(500).json({ message: "Interner Serverfehler" });
+	}
+  });
+
+  router.get("/speed/:id", async (req, res) => {
+	try {
+		let speedData = [
+			{ time: 0, speed: 7 },
+			{ time: 1, speed: 8 },
+			{ time: 2, speed: 9 },
+			{ time: 3, speed: 9 },
+			{ time: 4, speed: 9 },
+			{ time: 5, speed: 9 },
+		];
+		
+		let currentSpeed = 9; // Ausgangspunkt für Variation
+		for (let time = 6; time <= 60; time++) {
+			// Füge Variation hinzu, indem ein zufälliger Wert zwischen -1 und 1 zum aktuellen Speed hinzugefügt wird
+			let variation = (Math.random() * 2) - 1;
+			currentSpeed += variation;
+			speedData.push({ time: time, speed: currentSpeed });
+		}
+		
+	  res.status(200).json(speedData);
+	} catch (error) {
+	  res.status(500).json({ message: "Interner Serverfehler" });
+	}
+  });
+
 export default router;
